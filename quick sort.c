c7261ced341e97188fd12e0459e27bfb34988090
GUNA <gunavardhan000@gmail.com>
2023-09-24 11:30:17 +0530

    fixed

--- quick sort.c
+++ quick sort.c
@@ -1,38 +1,40 @@
 #include<stdio.h>
-void quicksort(int number[25],int first,int last){
-    int i, j, pivot, temp;
-    if(first<last){
-        pivot=first;
-        i=first;
-        j=last;
-        for (int i=0;i<j;i++){
-            while(number[i]<=number[pivot]&&i<last)
-            i++;
-            while(number[j]>number[pivot])
-            j--;
-            if(i<j){
-                temp=number[i];
-                number[i]=number[j];
-                number[j]=temp;
-            }
-        }
-        temp=number[pivot];
-        number[pivot]=number[j];
-        number[j]=temp;
-        quicksort(number,first,j-1);
-        quicksort(number,j+1,last);
-    }
+int swap(int *a , int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+void quicksort(int arr[25],int first,int last){
+   int i, j, pivot, temp;
+   if(first<last){
+      pivot=first;
+      i=first;
+      j=last;
+      while(i<j){
+         while(arr[i]<=arr[pivot]&&i<last)
+         i++;
+         while(arr[j]>arr[pivot])
+         j--;
+         if(i<j){
+            swap(&arr[i],&arr[j]);
+         }
+      }
+      swap(&arr[pivot],&arr[j]);
+      quicksort(arr,first,j-1);
+      quicksort(arr,j+1,last);
+   }
 }
 int main(){
-    int i, count, number[25];
-    printf("How many elements will you enter: ");
-    scanf("%d",&count);
-    printf("Enter %d elements: ", count);
-    for(i=0;i<count;i++)
-    scanf("%d",&number[i]);
-    quicksort(number,0,count-1);
-    printf("Order of Sorted elements: ");
-    for(i=0;i<count;i++)
-    printf(" %d",number[i]);
-    return 0;
+   int i, n, arr[25];
+   printf("How many elements will you enter: ");
+   scanf("%d",&n);
+   printf("Enter %d elements: ", n);
+   for(i=0;i<n;i++)
+   scanf("%d",&arr[i]);
+   quicksort(arr,0,n-1);
+   printf("Order of Sorted elements: ");
+   for(i=0;i<n;i++)
+   printf(" %d",arr[i]);
+   return 0;
 }
